822a56e7fb7289f95dd2e37c7869008fc10afc07
changethislaterforsure <benjmain.legge@keyin.comm>
2025-05-29 16:18:33 -0230

    Finished problem 1

---
+++ Problem 1/TestMyLine.java
@@ -0,0 +1,12 @@
+public class TestMyLine {
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(1, 2);
+        MyPoint p2 = new MyPoint(4, 6);
+
+        MyLine line = new MyLine(p1, p2);
+
+        System.out.println("My Line: " + line);
+        System.out.println("Length: " + line.getLength());
+        System.out.println("Gradient (radians): " + line.getGradient());
+    }
+}
